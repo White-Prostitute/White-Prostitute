c98662c77eab72c07ef2c89585b279614ec5af20
liguohua <3537136394@qq.com>
2023-03-03 09:45:11 +0800

    修改命名

--- StockSystem/src/main/java/com/example/stocksystem/dao/UserDao.java
+++ StockSystem/src/main/java/com/example/stocksystem/dao/UserDao.java
@@ -3,7 +3,12 @@ package com.example.stocksystem.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.stocksystem.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserDao extends BaseMapper<User> {
+
+    @Select("select * from user where user_id = #{user_id}")
+    User getOne(Integer user_id);
+
 }
--- StockSystem/src/main/java/com/example/stocksystem/entity/User.java
+++ StockSystem/src/main/java/com/example/stocksystem/entity/User.java
@@ -1,51 +1,15 @@
 package com.example.stocksystem.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
 public class User {
 
-    int user_id;
-    String user_name;
-    String user_password;
-    String user_phone_num;
+    @TableId
+    Integer userId;
+    String userName;
+    String userPassword;
+    String userPhoneNum;
 
-    public User(int user_id, String user_name, String user_password, String user_phone_num) {
-        this.user_id = user_id;
-        this.user_name = user_name;
-        this.user_password = user_password;
-        this.user_phone_num = user_phone_num;
-    }
-
-    public User() {
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getUser_password() {
-        return user_password;
-    }
-
-    public void setUser_password(String user_password) {
-        this.user_password = user_password;
-    }
-
-    public String getUser_phone_num() {
-        return user_phone_num;
-    }
-
-    public void setUser_phone_num(String user_phone_num) {
-        this.user_phone_num = user_phone_num;
-    }
 }
